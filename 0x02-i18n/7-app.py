216944b6fa48cf2e33e763f85b2a425bd4b315ac
Timothy Kibirige <tmwesigwa2@gmail.com>
2024-02-09 14:18:16 +0000

    Infer appropriate time zone

--- 0x02-i18n/7-app.py
+++ 0x02-i18n/7-app.py
@@ -21,6 +21,8 @@ app = Flask(__name__)
 app.config.from_object(Config)
 app.url_map.strict_slashes = False
 babel = Babel(app)
+
+
 users = {
     1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
     2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
