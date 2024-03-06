8b6eb181f92c50f184d3340ba257d9371c742f43
mwesigwa2 <tmwesigwa2@gmail.com>
2024-03-06 14:18:25 +0000

    Node Redis client and advanced operations

---
+++ 0x03-queuing_system_in_js/4-redis_advanced_op.js
@@ -0,0 +1,40 @@
+import { createClient, print } from 'redis';
+
+const client = createClient();
+
+client
+  .on("connect", () => {
+    console.log("Redis client connected to the server");
+  })
+  .on("error", (error) => {
+    console.log(`Redis client not connected to the server: ${error}`);
+  });
+
+// Function to set multiple fields in a hash
+function setHashFields(hashName, fields) {
+  Object.entries(fields).forEach(([field, value]) => {
+    client.hset(hashName, field, value, print);
+  });
+}
+
+// Define the hash fields and their values
+const hashFields = {
+  'Portland': 50,
+  'Seattle': 80,
+  'New York': 20,
+  'Bogota': 20,
+  'Cali': 40,
+  'Paris': 2
+};
+
+// Create Hash
+setHashFields('HolbertonSchools', hashFields);
+
+// Display Hash
+client.hgetall('HolbertonSchools', (err, reply) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(reply);
+});
