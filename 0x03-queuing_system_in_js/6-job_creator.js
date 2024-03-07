023be3f5a240975955a702924f60e205cf4a62e8
mwesigwa2 <tmwesigwa2@gmail.com>
2024-03-07 02:51:18 +0000

    Create the Job creator

---
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,29 @@
+import { createQueue } from 'kue';
+var kue = require('kue')
+   ,queue = kue.createQueue();
+
+// Create an object containing the job data
+const jobData = {
+  phoneNumber: '0777698976',
+  message: 'Hello there from your notification!',
+};
+
+// Create a queue named push_notification_code and create a job with the provided data
+const job = queue.create('push_notification_code', jobData)
+  .save((err) => {
+    if (err) {
+      console.error('Notification job failed');
+      return;
+    }
+    console.log('Notification job created:', job.id);
+  });
+
+// Listen for job completion
+job.on('complete', () => {
+  console.log('Notification job completed');
+});
+
+// Listen for job failure
+job.on('failed', () => {
+  console.log('Notification job failed');
+});
